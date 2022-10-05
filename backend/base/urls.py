cea39ef857228db76afd0a2fa70c0fa268147fe6
Shakl0ne <934803719@qq.com>
2022-10-05 14:31:16 +0300

    rearranged backend

--- backend/base/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('products/', views.getProducts, name="products"),
-
-    path('products/<str:pk>/', views.getProduct, name="product"),
-]
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,10 +2,26 @@ import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { productListReducers } from "./reducers/productReducers";
-
+import {
+    productListReducer,
+    productDetailsReducer,
+    productDeleteReducer,
+    productCreateReducer,
+    productUpdateReducer,
+    productReviewCreateReducer,
+    productTopRatedReducer,
+} from './reducers/productReducers'
 const reducer = combineReducers({
-    productList: productListReducers,
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
+    productTopRated: productTopRatedReducer,
+
 })
+
 
 const initialState = {}
 
