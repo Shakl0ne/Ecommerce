a1bf997d8ce311b45a12eff65cc7d4d48533efed
Shakl0ne <934803719@qq.com>
2023-01-08 00:15:06 +0200

    fixed review serializer

--- base/serializers.py
+++ base/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-from .models import Product
+from .models import Product, Review
+
 
 class ProductSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField(read_only=True)
+
     class Meta:
         model = Product
+        fields = '__all__'
+
+    def get_reviews(self, obj):
+        reviews = obj.review_set.all()
+        serializer = ReviewSerializer(reviews, many=True)
+        return serializer.data
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
         fields = '__all__'
