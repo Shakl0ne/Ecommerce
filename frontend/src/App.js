5c38c011c95b393ccc90008e9e4877b8f3810793
Shakl0ne <934803719@qq.com>
2023-01-09 01:36:48 +0200

    added place order screen, order screen, payment screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,10 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
-
+import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+import PlaceorderScreen from "./screens/PlaceorderScreen"
+import OrderScreen from "./screens/OrderScreen"
 function App() {
     return (
         <BrowserRouter>
@@ -25,8 +28,10 @@ function App() {
                         <Route path='/login' element={<LoginScreen/>}/>
                         <Route path='/register' element={<RegisterScreen/>}/>
                         <Route path='/profile' element={<ProfileScreen/>}/>
-
-
+                        <Route path='/shipping' element={<ShippingScreen/>}/>
+                        <Route path='/payment' element={<PaymentScreen/>}/>
+                        <Route path='/placeorder' element={<PlaceorderScreen/>}/>
+                        <Route path='/order/:id' element={<OrderScreen/>}/>
                     </Routes>
                 </Container>
             </main>
--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { Form, Button, Row, Col, Table } from 'react-bootstrap'
 import { LinkContainer } from 'react-router-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
