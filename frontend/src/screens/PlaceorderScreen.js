48908dfb89aad532d18a371961324f219587f72d
Shakl0ne <934803719@qq.com>
2023-01-09 23:07:07 +0200

    no real payment on place order screen

--- frontend/src/screens/PlaceorderScreen.js
+++ frontend/src/screens/PlaceorderScreen.js
@@ -151,6 +151,7 @@ function PlaceOrderScreen() {
                                     disabled={cart.cartItems === 0}
                                     onClick={placeOrder}
                                 >
+                                    {/*this button automatically mark as paid*/}
                                     Place Order
                                 </Button>
                             </ListGroup.Item>
